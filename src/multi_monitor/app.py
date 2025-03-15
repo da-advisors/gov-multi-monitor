92a67b57aa007c29989d7ea6a88ae84ca5771d82
Matt Gawarecki <matt.gawarecki@gmail.com>
2025-03-14 23:44:30 -0500

    Spruce up resource list, previews, and details

--- src/multi_monitor/app.py
+++ src/multi_monitor/app.py
@@ -1,72 +1,76 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
+
 from .db import MonitorDB
 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
 app = Flask(__name__)
 
-db = MonitorDB('../data/monitor.db', read_only=True)
+db = MonitorDB("../data/monitor.db", read_only=True)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def landing_home():
+
+@app.route("/")
+def view_landing():
     results = db._read_query(
         """
         SELECT count(*) as data_resources_count FROM resources
-        """)
+        """
+    )
 
-    data_resources_count = results[0]['data_resources_count']
+    data_resources_count = results[0]["data_resources_count"]
 
     return render_template(
-        'multi_page/landing_page.html',
+        "multi_page/landing_page.html.jinja",
         data_collections_count=100,
         data_resources_count=data_resources_count,
         unavailable_collections_count=1,
         partially_unavailable_collections_count=2,
-        stale_collections_count=3
+        stale_collections_count=3,
     )
 
-@app.route('/resources/data')
-def list_resources():
+
+@app.route("/status/")
+def list_statuses():
+    return render_template("index.html.jinja", tags=None, results=None)
+
+
+@app.route("/resources/_data")
+def get_raw_resources():
     results = db._read_query(
         """
         SELECT * FROM resources
-        """)
+        """
+    )
     return results
 
-@app.route('/resources/')
-def render_resources_list_page():
-    results = list_resources()
-    return render_template(
-        "multi_page/resource_list.html",
-        resources=results
-    )
 
-@app.route('/resources/<resource_id>')
-def show_resource_details(resource_id: str):
-    # TODO: Make as safer executition string.
+@app.route("/resources/")
+def list_resources():
+    results = get_raw_resources()
+    return render_template("multi_page/resource_list.html.jinja", resources=results)
+
+
+@app.route("/resources/<resource_id>")
+def view_resource(resource_id: str):
+    # TODO: Make as safer execution string.
     results = db._read_query(
-    f"""
+        f"""
     SELECT * FROM resources
     WHERE id = '{resource_id}';
     """
     )
 
-    status_history = get_status_history_for_resource(resource_id)
+    status_history = view_resource_status_history(resource_id)
 
     return render_template(
-        "multi_page/resource_detail.html",
+        "multi_page/resource_detail.html.jinja",
         resource=results[0],
-        status_history=status_history
+        status_history=status_history,
     )
 
-@app.route('/resources/<resource_id>/status_history')
-def get_status_history_for_resource(resource_id: str):
+
+@app.route("/resources/<resource_id>/status_history")
+def view_resource_status_history(resource_id: str):
     results = db._read_query(
-    f"""
+        f"""
     SELECT * FROM resource_status
     WHERE resource_id = '{resource_id}'
     ORDER BY checked_at DESC;
@@ -76,11 +80,5 @@ def get_status_history_for_resource(resource_id: str):
     return results
 
 
-
-
-# main driver function
-if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server.
+if __name__ == "__main__":
     app.run()
